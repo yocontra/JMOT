4a70a86905d818a4524468a3056e453f3f678445
Contra <contra@maricopa.edu>
2011-02-05 16:34:02 -0700

    quick fix to allow custom arg names

--- src/net/contra/obfuscator/Settings.java
+++ src/net/contra/obfuscator/Settings.java
@@ -1,9 +1,10 @@
 package net.contra.obfuscator;
 
 public class Settings {
-    public static final double Version = 0.10;
+    public static final double Version = 0.11;
     public static final boolean Debug = true;
 
     public static final String CipherName = "hax";
+    public static final String CipherArg = "s";
     public static final int CipherKey = 127;
 }
